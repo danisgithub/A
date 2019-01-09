d3839595c955603a275d2ecabf16bc2028a7f272
dani <n1158754745@gmail.com>
2019-01-09 19:26:06 +0800

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,19 @@
+h = input('身高（M）：')
+w = input('体重（KG)：')
+
+hh = float(h)
+ww = float(w)
+
+bmi = ww/(hh*hh)
+
+if float(bmi) > 32:
+    print('严重肥胖')
+elif 28 < float(bmi) <= 32:
+    print('肥胖')
+elif 25 < float(bmi) <=28:
+    print('过重')
+elif 18.5 < float(bmi) <= 25:
+    print('正常')
+else:
+	print('过轻')
+	
