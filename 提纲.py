841095a2e6c81903b67d3188f9f49d3450ccb815
dani <n1158754745@gmail.com>
2019-01-10 00:07:00 +0800

    Update 提纲.py

--- 提纲.py
+++ 提纲.py
@@ -36,7 +36,7 @@ while n <= 100:    #输出1-100
     n = n + 1
 print('end')
 
-#break    停止，需借助if条件判断
+#break停止，需借助if条件判断
 n = 1
 while n <= 100:
     if n > 10: 
@@ -45,7 +45,7 @@ while n <= 100:
     n = n + 1
 print('end')
 
-#continue    跳过循环
+#continue跳过循环
 n = 0
 while n < 10:
     n = n + 1
@@ -53,5 +53,43 @@ while n < 10:
     continue
 print(n)    #此程序输出10以内的奇数
 
+#{}
 #dict    dictionary字典
+#其他语言中也称为map，使用键-值（key-value）存储，具有极快的查找速度。
+d = {'one':1,'two':2,'three':3}
+d['a'] = 4    #直接插入
+'b' in d    #查看b是否存在于d，返回布尔值
+d.pop('a')    #删除，pop()
+#set可以看成数学意义上的无序和无重复元素的集合，借助list
+s = set([1,2,3,4])
+s.add(5)
+s.remove(5)
 
+#函数
+#https://docs.python.org/3/library/functions.html
+a = abs    #变量a指向abs函数
+#定义函数
+def jdz(a):
+    if a >= 0:
+	    return a
+	else:
+	    return -a
+
+#空函数
+def none():
+    pass    #pass可用来占位，让程序先运行
+
+#数据类型检查函数isinstance()
+if not isinstance(x, (int, float)):    
+    raise XXXXXXXXX    #如果x的类型不是整数或浮点数，则报错
+
+#导入math包，可以使用sin cos
+import math
+
+#从一个点到另一个点 xy坐标，位移，角度
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+x,y = move()
+print(x,y)
