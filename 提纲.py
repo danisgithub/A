ff61d76d2c6cfc7bf933e5817a5df9a926c309b0
dani <n1158754745@gmail.com>
2019-01-09 21:01:08 +0800

    Add files via upload

---
+++ 提纲.py
@@ -0,0 +1,40 @@
+#Python练习
+#print
+print ('1024 * 768 =', 1024*786)
+
+#变量赋值与input
+name = input('your name:')
+print ('hello',name)
+
+#list and  tuple
+group = [1,2,3]    #可修改
+num = (1,2,3)    #不可修改
+
+#条件判断    if elif else
+if    :
+    print
+else    :
+    print
+else:
+    print    
+	
+#循环
+#for in 循环
+name = ['one','two','three']
+for x in name:
+    print(x)
+	
+sum = 0    #100累加
+for x in rang(101):
+    sum = sum + x
+print (sum)
+    
+#while循环
+n = 1    
+while n <= 100:    #输出1-100
+    print(n)
+	n = n + 1
+print('end')
+
+#break
+	
