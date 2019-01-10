13df48a26d9d4b549cd5de8d8e3469fdd7924c01
dani <n1158754745@gmail.com>
2019-01-10 13:38:51 +0800

    Update fangcheng.py

--- fangcheng.py
+++ fangcheng.py
@@ -3,19 +3,20 @@
 import math
 def quad(a,b,c):
     d = b*b-4*a*c
-	if d < 0:
-	    print('no')
-	else:
+    if d < 0:
+	    print('no')    #还需再处理
+    else:
 	    x1 = (-b+math.sqrt(d))/(2*a)
-		x2 = (-b-math.sqrt(d))/(2*a)
-		return(x1,x2)
+	    x2 = (-b-math.sqrt(d))/(2*a)
+	    return(x1,x2)
 
-a = input('a')
-b = input('b')
-c = input('c')
+a = float(input('a:'))
+b = float(input('b:'))
+c = float(input('c:'))
 
 f = quad(a,b,c)
 
-print('f')
+print(f)
 
-#在IDLE中代码运行正常，但似乎格式缩进有问题（line6），保存后无法正确执行，待查。
+#在IDLE中代码运行正常，但似乎格式缩进有问题（line8），保存后无法正确执行，待查。
+#恢复正常
